1b3b7e1d3d842c41e74b4516d75363074d33dbca
Benoit Kohler <bkohler@bearchitek.tailc018d.ts.net>
2024-11-27 10:41:22 +0100

    split usage of query

--- transforms/device_cisco_python.py
+++ transforms/device_cisco_python.py
@@ -2,15 +2,47 @@ from infrahub_sdk.transforms import InfrahubTransform
 
 
 class DeviceCisco(InfrahubTransform):
-    query = "device_info"
+    query = "device_info2"
 
     async def transform(self, data):
         response = {}
 
         for device in data["InfraDevice"]["edges"]:
+            device_name = device["node"]["name"]["value"]
+            response[device_name] = {"interfaces": {}}
+
             for intf in device["node"]["interfaces"]["edges"]:
                 intf_name = intf["node"]["name"]["value"]
-                intf_description = intf["node"]["description"]["value"]
-                response[intf_name] = intf_description
+                intf_data = {}
+
+                # Interface description
+                intf_data["description"] = intf["node"].get("description", {}).get("value", "FREE")
+
+                # Interface status (enabled/disabled)
+                intf_data["enabled"] = intf["node"].get("enabled", {}).get("value", True)
+
+                # # IP addresses assigned to the interface
+                # ip_addresses = []
+                # if intf["node"].get("ip_addresses"):
+                #     for ip_edge in intf["node"]["ip_addresses"]["edges"]:
+                #         ip_addresses.append(ip_edge["node"]["address"]["value"])
+                # intf_data["ip_addresses"] = ip_addresses
+
+                # # Interface role for OSPF configuration
+                # intf_data["role"] = intf["node"].get("role", {}).get("value", None)
+
+                # # VLAN information
+                # if intf["node"].get("untagged_vlan") and intf["node"]["untagged_vlan"].get("node"):
+                #     intf_data["untagged_vlan"] = intf["node"]["untagged_vlan"]["node"]["vlan_id"]["value"]
+
+                # if intf["node"].get("tagged_vlan") and intf["node"]["tagged_vlan"].get("edges"):
+                #     tagged_vlans = [
+                #         vlan_edge["node"]["vlan_id"]["value"]
+                #         for vlan_edge in intf["node"]["tagged_vlan"]["edges"]
+                #     ]
+                #     intf_data["tagged_vlans"] = tagged_vlans
+
+                # Add interface data to response
+                response[device_name]["interfaces"][intf_name] = intf_data
 
         return response
